6cfc412c7897931972bb604193f3d2bc587d1392
Daw-Chih Liou <dawochih.liou@gmail.com>
2021-11-11 07:30:30 +0100

    feat:
    traits: defining shared behavior

--- the-rust-programming-language/traits-defining-shared-behavior/src/main.rs
+++ the-rust-programming-language/traits-defining-shared-behavior/src/main.rs
@@ -1,44 +1,62 @@
-fn main(){
-    // Traits are similar to interfaces in other languages. Some differences.
-    pub trait Summary {
-        fn summarize_author(&self) -> String;
+/*
+ * Traits are a way share functionality of a type with other types.
+ * Similar to interfaces in other languages with some differences.
+ *
+ * To define a trait:
+ *
+ * ```
+ *  pub trait TraitName {
+ *      fn method_name(&self) -> ReturnType;
+ *      fn another_method(&self) -> ReturnType {
+ *          // default implementation
+ *      }
+ *  }
+ * ```
+ *
+ * To implement a trait on a type:
+ *
+ * ```
+ *  pub struct AType {
+ *      pub name: String,
+ *  }
+ *
+ *  impl TraitName for AType {
+ *      fn method_name(&self) -> ReturnType {
+ *          // implementation
+ *
+ *          // a method uses another method
+ *          println!("{}", self.another_method());
+ *      }
+ *  }
+ * ```
+ *
+ * A Trait can be used as a parameter type or a return type.
+ *
+ * We can use "impl Trait" syntax or trait bound syntax when
+ * using a trait as parameter. We can use "+" syntax and "where" clauses
+ * to express multiple trait bounds.
+ *
+ * We can use trait bounds to implement method conditionally, similar to
+ * "overload" in some languages.
+ */
 
-        fn summarize(&self) -> String {
-            format!("(Read more from {}...)", self.summarize_author())
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
+    largest
+}
 
-    pub struct NewsArticle {
-        pub headline: String,
-        pub location: String,
-        pub author: String,
-        pub content: String,
-    }
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    impl Summary for NewsArticle {
-        fn summarize_author(&self) -> String {
-            format!("{}", self.author)
-        }
-    }
-
-    pub struct Tweet {
-        pub username: String,
-        pub content: String,
-        pub reply: bool,
-        pub retweet: bool,
-    }
-
-    impl Summary for Tweet {
-        fn summarize_author(&self) -> String {
-            format!("@{}", self.username)
-        }
-    }
-
-    let tweet = Tweet {
-        username: String::from("horse_ebooks"),
-        content: String::from("of course, as you probably already know, people"),
-        reply: false,
-        retweet: false,
-    };
-    println!("1 new tweet: {}", tweet.summarize())
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
