c91bf7c1bb939f95d569ea13f0edf85be1b816a2
Daw-Chih Liou <dawochih.liou@gmail.com>
2021-11-12 07:13:20 +0100

    feat: lifetimes

--- the-rust-programming-language/validating-references-with-lifetimes/src/main.rs
+++ the-rust-programming-language/validating-references-with-lifetimes/src/main.rs
@@ -12,33 +12,36 @@
  *
  * "Ultimately, lifetime syntax is about connecting the lifetimes
  * of various parameters and return values of functions."
- * 
- * Structs can hold references. Each reference should add a 
+ *
+ * Structs can hold references. Each reference should add a
  * lifetime annotation. For example:
- * 
+ *
  * ```
- *  // The struct has a string slice reference. It means the struct 
+ *  // The struct has a string slice reference. It means the struct
  *  // won't outlive the reference.
- * 
+ *
  *  struct ImportantExcerpt<'a> {
  *      part: &'a str,
  *  }
  * ```
- * 
- * Lifetime elision rules are the common usecases that are programmed 
- * into Rust compiler. For lifetimes, the compiler considers the 
- * input lifetimes(function parameters) and output lifetimes(return 
+ *
+ * Lifetime elision rules are the common usecases that are programmed
+ * into Rust compiler. For lifetimes, the compiler considers the
+ * input lifetimes(function parameters) and output lifetimes(return
  * values). If it is deterministic, it will compile.
- * 
+ *
  * Three lifetime elision rules
- * 
+ *
  *  1. Each parameter gets its own lifetime parameter.
- *  2. If there's only one input lifetime paramter, the lifetime is 
+ *  2. If there's only one input lifetime paramter, the lifetime is
  *     assigned to all output lifetime parameters.
- *  3. If one of the input lifetime parameters is `&self` or `&mut self`, 
+ *  3. If one of the input lifetime parameters is `&self` or `&mut self`,
  *     the lifetime of self is assigned to all output lifetime paramters.
- * 
- * 
+ *
+ * `'static` is a special lifetime. It tells that the reference lives for
+ * the entire duration of the program.
+ *
+ *
  */
 
 // Enforce &x, &y, and the reference of the return slice to
@@ -50,10 +53,32 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
         y
     }
 }
+
+/*
+ * generic type prarameters, trait bounds, and lifetimes togehter
+ */
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
+
+    let another_result =
+        longest_with_an_announcement(string1.as_str(), string2, "Today is someone's birthday!");
+    println!("The longest string is {}", another_result);
 }
